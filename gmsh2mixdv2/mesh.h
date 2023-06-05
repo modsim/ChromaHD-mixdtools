fc1373a5d814f01cb02bcd84ef95450ac8af6d6d
Jayghosh Rao <jayghoshter@gmail.com>
2023-06-05 08:25:37 +0200

    gmsh2mixdv2: parallel reads

--- gmsh2mixdv2/mesh.h
+++ gmsh2mixdv2/mesh.h
@@ -49,9 +49,8 @@ class Mesh
             }
             std::sort(sortedNodes.begin(), sortedNodes.end());
 
+            #pragma omp critical
             trimap.insert(std::pair<std::vector<size_t>, Triangle *>(sortedNodes, new Triangle(line)) );
-
-
 
         }
 };
